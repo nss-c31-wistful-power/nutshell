9d6f2681cdacc9fdad8c13dfb54f0eb19a329837
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-04 13:05:12 -0500

    Added more notes

--- src/scripts/events/eventsForm.js
+++ src/scripts/events/eventsForm.js
@@ -1,8 +1,10 @@
-// keep track of future event
 // click affordance (button) to enter new event
 // present form upon button click - has three fields: name of event (input), date of event (calendar), location of event (input)
-// after details are entered, save ability - button - PUT/PATCH request
-// display container - get and update
+// after details are entered, save ability - button - POST request
+// display container - get updated information - "events component"
+// when component is updated, next event on agenda should have bold text, be slightly larger in size, and have a non-white, non-offensive background color (in CSS or created in JS files?)
+// edit function - button pressed, brings up form with prepopulated information in fields
+// save button - updates information with PUT/PATCH (is there a way to use a PUT rather than a PATCH? Will it affect the data?)
 
 export default {
 
