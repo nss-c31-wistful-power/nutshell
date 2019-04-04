2dae2e5b1deef1f36b12996f63d22809efb67159
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-04 11:57:28 -0500

    Taking notes part one

---
+++ src/scripts/events/eventsForm.js
@@ -0,0 +1,11 @@
+// keep track of future event
+// click affordance (button) to enter new event
+// present form upon button click - has three fields: name of event (input), date of event (calendar), location of event (input)
+// after details are entered, save ability - button - PUT/PATCH request
+// display container - get and update
+
+export default {
+
+
+
+}
