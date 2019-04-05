3aeaf21e41c4350a2e15e956a1b35dc013a91c0c
JMONDESIR <Joey.M.Dev@gmail.com>
2019-04-05 13:20:28 -0500

    working on edit and delete

--- src/scripts/Messages/DOMhandler.js
+++ src/scripts/Messages/DOMhandler.js
@@ -10,7 +10,7 @@ const messageBuilder = {
                 messageContainer.appendChild(messageHeader)
 
                 // Message Form builder
-                const message_formWrapper = document.createElement("fieldset")
+                const message_formWrapper = document.createElement("div")
                 message_formWrapper.classList = "message_formWrapper"
 
                 const message_Input = document.createElement("input")
@@ -20,7 +20,7 @@ const messageBuilder = {
                 message_sendButton.textContent = "SEND"
                 message_sendButton.addEventListener("click", handleMessages.sendMessage)
 
-                const message_Output = document.createElement("textarea")
+                const message_Output = document.createElement("div")
                 message_Output.id = "message_Output"
 
                 message_formWrapper.appendChild(message_Input)
@@ -30,8 +30,25 @@ const messageBuilder = {
         },
         displayMessages() {
                 messageAPI.getMessages()
-                        .then(data => data.forEach(message => {
-                                console.log(message)
+                        .then(data => data.forEach(entry => {
+                                const messageContainer = document.querySelector("#messages-section")
+                                const message_Output = document.querySelector("#message_Output")
+                                const messageCard = document.createElement("div")
+                                const userMessage = document.createElement("p")
+                                userMessage.textContent = entry.messages
+
+                                const eraser = document.createElement("button")
+                                eraser.id = "eraser"
+
+                                const editor = document.createElement("button")
+                                editor.id = "editor"
+
+                                messageContainer.appendChild(messageCard)
+                                messageCard.appendChild(userMessage)
+                                message_Output.appendChild(userMessage)
+                                message_Output.appendChild(editor)
+                                message_Output.appendChild(eraser)
+
                         }))
         }
 }
