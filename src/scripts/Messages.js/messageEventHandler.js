837e51d24af02e97c19f094a9462f7d388d47442
JMONDESIR <Joey.M.Dev@gmail.com>
2019-04-05 11:03:45 -0500

    removed directories

--- src/scripts/Messages.js/messageEventHandler.js
+++
@@ -1,8 +0,0 @@
-const handleMessages = {
-        sendMessage() {
-                const messageInput = document.querySelector("#message_Input")
-                console.log(messageInput.value)
-        }
-}
-
-export default handleMessages
