0524c8d179bc218532d71adf43740cb469cb1b33
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-05 10:14:29 -0500

    Building more elements

--- src/scripts/events/eventsForm.js
+++ src/scripts/events/eventsForm.js
@@ -1,5 +1,5 @@
 // click affordance (button) to enter new event
-// present form upon button click - has three fields: name of event (input), date of event (calendar), location of event (input)
+// present form upon button click - has three fields: name of event (input), date of event (calendr), location of event (input)
 // after details are entered, save ability - button - POST request
 // display container - get updated information - "events component"
 // when component is updated, next event on agenda should have bold text, be slightly larger in size, and have a non-white, non-offensive background color (in CSS or created in JS files?)
@@ -10,23 +10,36 @@ import postEventsToList from "./eventsCalls"
 
 export default {
     buildEventForm() {
+
+        const eventContainer = document.createElement("div")
+
         // affordance for new event - button which will need to create all the following fields
         const newEventButton = document.createElement("button")
         newEventButton.textContent = "Add New Event"
         
-        
         // fields for created forms 
         const eventNameLabel = document.createElement("label")
         eventNameLabel.textContent = "New Event Name"
-
+        eventContainer.appendChild(eventNameLabel)
+        
         const eventNameInput = document.createElement("input")
+        eventContainer.appendChild(eventNameInput)
 
-        const eventDate = document.createElement("")
+        // calendar feature? 
+        // const eventDate = document.createElement("")
+        // eventContainer.appendChild(eventDate)
+
 
         const eventLocationInput = document.createElement("input")
+        eventContainer.appendChild(eventLocationInput)
 
         const saveEventButton = document.createElement("button")
         saveEventButton.addEventListener("click", postEventsToList())
+
+        
+        
+        
+        
     },
     
 }
